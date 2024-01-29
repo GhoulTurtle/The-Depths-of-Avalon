2e795705832ea78c6e2169aa6cdba617e845a071
chuss <cal.bobbo@gmail.com>
2024-01-28 20:36:28 -0600

    Got the different damage types set up
    Need to go through and code how they deal damage still but their SO's are set up.

---
+++ Assets/Scripts/ScriptableObjects/DamageTypes/ChargeDamageSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeDamageSO : DamageTypeSO
+{
+    public override void DealDamage(HealthSystem healthSystem, float damageAmount) {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/DamageTypes/IceDamageSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceDamageSO : DamageTypeSO
+{
+    // Instant Damage.
+    public override void DealDamage(HealthSystem healthSystem, float damageAmount) {
+        
+    }
+}
