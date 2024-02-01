98a4ceaf6a08bec40fef91c1adf662ba56246922
chuss <cal.bobbo@gmail.com>
2024-01-31 19:36:13 -0600

    All damage types created and working basically.
    Got basic implementation of all the damage types working.

--- Assets/Scripts/ScriptableObjects/DamageTypes/ChargeDamageSO.cs
+++ Assets/Scripts/ScriptableObjects/DamageTypes/ChargeDamageSO.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Charge Damage", menuName = "Damage Type/Charge Damage")]
 public class ChargeDamageSO : DamageTypeSO
 {
     public override void DealDamage(HealthSystem healthSystem, float damageAmount) {
-        
+        healthSystem.TakeDamage(damageAmount);
     }
 }
--- Assets/Scripts/ScriptableObjects/DamageTypes/IceDamageSO.cs
+++ Assets/Scripts/ScriptableObjects/DamageTypes/IceDamageSO.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Ice Damage", menuName = "Damage Type/Ice Damage")]
 public class IceDamageSO : DamageTypeSO
 {
     // Instant Damage.
     public override void DealDamage(HealthSystem healthSystem, float damageAmount) {
-        
+        healthSystem.TakeDamage(damageAmount);
     }
 }
