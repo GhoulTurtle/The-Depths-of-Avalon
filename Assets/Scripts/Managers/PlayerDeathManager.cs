0c18e39f5b041b00c9ff6fbb401a93cc4212a9f6
chuss <cal.bobbo@gmail.com>
2024-02-09 16:34:31 -0600

    continuing work on Respawn and death

--- Assets/Scripts/Managers/PlayerDeathManager.cs
+++ Assets/Scripts/Managers/PlayerDeathManager.cs
@@ -6,10 +6,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerDeathManager : MonoBehaviour {
+
     private PlayerManager playerManager => GetComponent<PlayerManager>();
     public List<Player> playerList = new List<Player>();
-    public bool IsOnePlayerDead;
-    [SerializeField] private float respawnTimer;
 
     private void OnEnable() {
         playerManager.OnCorrectPlayerCount += CreatePlayerList;
@@ -19,48 +18,13 @@ public class PlayerDeathManager : MonoBehaviour {
         playerManager.OnCorrectPlayerCount -= CreatePlayerList;
     }
 
-    private void Update() {
-
-    }
-
     private void CreatePlayerList(object sender, EventArgs e) {
         foreach(Player player in playerManager.CurrentPlayerList) {
             playerList.Add(player);
             HealthSystem healthSystem = player.playerCharacter.GetComponent<HealthSystem>();
             if(healthSystem != null) {
-                Debug.Log(player + " Has health system");
-                healthSystem.OnDie += PlayerDied;
+                Debug.Log(player.playerNum + " Has health system");
             }
         }
-    }
-
-    private void PlayerDied(object sender, EventArgs e) {
-        HealthSystem healthSystem = sender as HealthSystem;
-
-        if(healthSystem != null) {
-            if(IsOnePlayerDead) {
-                Time.timeScale = 0f;
-                Debug.Log("Game Over");
-            } else {
-                Debug.Log("Player died, starting respawn");
-                IsOnePlayerDead = true;
-                Transform playerDeathSpot = healthSystem.gameObject.transform;
-                Debug.Log(playerDeathSpot.position);
-                StartCoroutine(PlayerRespawn(healthSystem.gameObject, respawnTimer, playerDeathSpot));
-            }
-        }
-    }
-
-    private IEnumerator PlayerRespawn(GameObject playerObject, float respawnDelay, Transform respawnPoint) {
-        yield return new WaitForSeconds(respawnDelay);
-
-        HealthSystem healthSystem = playerObject.GetComponent<HealthSystem>();
-        if(healthSystem != null) {
-            playerObject.transform.position = respawnPoint.position;
-            healthSystem.Heal(100);
-            Debug.Log(respawnPoint.position);
-        }
-
-        IsOnePlayerDead = false;
     }
 }
